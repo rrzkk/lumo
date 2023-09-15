fdefc25e2c03229673f0b38f017d22a544c440ca
ren <rrzkk@live.com>
2023-09-15 17:32:07 +1000

    responsive nav

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -4,11 +4,16 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <Link href={"/aboutus"}>About Us</Link>
-      <Link href={"/menu"}>Menu</Link>
-      <Link href={"/cateringmenu"}>Catering Menu</Link>
-      <Link href={"/contactus"}>Contact us</Link>
+    <div class="m-2 md:m-5 min-w-screen flex justify-center md:justify-between">
+      <div class="">
+        <Link href="/">logo</Link>
+      </div>
+      <div class="hidden md:block md:flex md:space-x-4">
+        <Link href={"/aboutus"}>About Us</Link>
+        <Link href={"/menu"}>Menu</Link>
+        <Link href={"/cateringmenu"}>Catering Menu</Link>
+        <Link href={"/contactus"}>Contact us</Link>
+      </div>
     </div>
   );
 };
