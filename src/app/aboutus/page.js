7e17149deda2c2023fadfb5574870f886fa88970
ren <rrzkk@live.com>
2023-10-28 23:19:11 +1100

    aboutus

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,7 +1,78 @@
+"use client";
+import React from "react"; // Import useState
+
+import BackgroundThree from "../components/backGroundThree";
+
 export default function aboutus() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <h1>About Us</h1>
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <div
+        style={{
+          position: "absolute",
+          height: "300vh",
+          width: "100vw",
+          zIndex: "0",
+        }}
+      >
+        <BackgroundThree />
+      </div>
+      <div className="about-us" style={{ zIndex: "500" }}>
+        <div className="about-us-inner">
+          <div className="about-us-section">
+            <div className="basis-2/5 flex justify-center content-center ">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-1.jpg"
+                alt="image description"
+              />
+            </div>
+            <div className="basis-3/5 flex  justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                The word "Lumos" comes from Harry Potter - a charm that
+                illuminated the tip of the caster’s wand, allowing the caster to
+                see in the dark. The idea of a bright light that led the way
+                appealed to us.
+              </p>
+            </div>
+          </div>
+          <div className="about-us-section">
+            <div className="basis-3/5 flex justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                Quality coffee & food can brighten your day, and a quality
+                espresso bar can brighten the world. Born from a mission to
+                pioneer what is possible in an industry moving at incredible
+                speed, Lumos is positioned to light the way.
+              </p>
+            </div>
+            <div className="basis-2/5 flex justify-center content-center">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-2.jpg"
+                alt="image description"
+              />
+            </div>
+          </div>
+          <div className="about-us-section">
+            <div className="basis-2/5 flex justify-center content-center">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-3.jpg"
+                alt="image description"
+              />
+            </div>
+            <div className="basis-3/5 flex justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                Leaving no part of our process in the shadows, our sustainable
+                approach shines a light on every person, product, and piece of
+                packaging involved from crop to cup. We embrace a responsibility
+                to deliver a transcendent sensory experience, whilst ensuring
+                our state-of-the-art setup and approach creates a better world
+                and lightens your day.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,190 +1,78 @@
 "use client";
-import React, { useState } from "react"; // Import useState
+import React from "react"; // Import useState
 
-import { Card } from "flowbite-react";
 import BackgroundThree from "../components/backGroundThree";
-import caterringMenu from "../components/caterringMenu";
-export default function test() {
-  const [isChecked, setIsChecked] = useState(true); // Initialize state
-  const handleToggleChange = () => {
-    setIsChecked(!isChecked); // Toggle the state value
-  };
 
+export default function test() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <div
         style={{
           position: "absolute",
-          height: "100vh",
+          height: "300vh",
           width: "100vw",
           zIndex: "0",
         }}
       >
         <BackgroundThree />
       </div>
-      <div className="menu-main " style={{ zIndex: "500" }}>
-        {/** Normal Menu */}
-        {isChecked && (
-          <div className="foodMenu">
-            <div className="basis-1/12 secTitile">All Day Breakfast</div>
-            <div
-              className="basis-5/12 foodSection"
-              style={{ borderLeft: " 2px solid #34c759 " }}
-            >
-              <p style={{ fontWeight: "bold" }}>Acai Bowl (V, GF, DF, ) 17</p>
-              <p>
-                100% organic Acai topped with fresh seasonal fruits & berries,
-                gluten free granola w/ coconut water
+      <div className="about-us" style={{ zIndex: "500" }}>
+        <div className="about-us-inner">
+          <div className="about-us-section">
+            <div className="basis-2/5 flex justify-center content-center ">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-1.jpg"
+                alt="image description"
+              />
+            </div>
+            <div className="basis-3/5 flex  justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                The word "Lumos" comes from Harry Potter - a charm that
+                illuminated the tip of the caster’s wand, allowing the caster to
+                see in the dark. The idea of a bright light that led the way
+                appealed to us.
               </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Classic Bacon & Egg Roll 13</p>
-              <p>
-                Grilled bacon, fried egg, hashbrown, aioli & BBQ sauce on milk
-                bun
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Signature Omelete</p>
-              <p>Tomato, baby spinach, onion with toast 14.5</p>
-              <p> w Ham or Bacon 17.5</p>
-              <p> w Smoked Salmon 18.5</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Eggs Benedict 19</p>
-              <p>
-                Poached eggs, bacon (or smoked salmon) & baby spinach on
-                sourdough with miso hollandaise sauce
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>BLAT 16</p>
-              <p>
-                Grilled rindless bacon, mixed lettuce, crushed avocado, tomato
-                w/aioli on Turkish
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Chilli Scrambled Eggs 17.5 </p>
-              <p>
-                Cherry tomato, baby spinach, XO sauce, chilli oil, garnish with
-                chilli flake & fresh chilli
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Eggs Your Way 14 </p>
-              <p>Two eggs as your way w/ sourdough toast</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Crushed Avo Stack (V,RSF) 16</p>
-              <p>
-                Crushed avocado, fetta cheese, cherry tomato & rocket on lightly
-                toasted turkish w/vinaigrette dressing
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>
-                Truffle Mushroom Avo Bruschetta (V) 20
-              </p>
-              <p>
-                Sauteed mushrooms, avocado, onion, truffle butter, fetta,
-                poached egg on toasted turkish bread
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>
-                Green Island Fritter (V) 16.5
-              </p>
-              <p>
-                Grilled homemade corn fritters, poached egg, crushed avocado,
-                tomato relish, tomato salsa and charred kale{" "}
-              </p>
-              <br />
-            </div>{" "}
-            <div className="basis-1/12 secTitile">Lunch</div>
-            {/** Here is the Lunch Menu */}
-            <div
-              className="basis-5/12 foodSection"
-              style={{ borderLeft: " 2px solid #34c759 " }}
-            >
-              <p style={{ fontWeight: "bold" }}>Chicken Halloumi Salad 18</p>
-              <p>
-                Grilled Chicken breast, halloumi, mesculin mix, cucumber,
-                radicchio, smashed avo, corn chips, Cherry tomato with French
-                dressing
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Lets Taco ‘bout Fish 19</p>
-              <p>
-                Crispy hoki, mixed salad, homemade mango salsa, tartare sauce on
-                flour tortillas
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Fish & Chips 18 </p>
-              <p>Crispy Hoki with steakhouse chips</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Fried Halloumi Burger 20</p>
-              <p>
-                Crispy halloumi cheese, coleslaw, bbq sauce, sliced tomato and
-                crispy onion on milk bun with steakhouse chips
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Angel Bay Beef Burger 20</p>
-              <p>
-                Homemade beef patty, bacon, cheese, pickled cucumber, aioli,
-                mixed salad, tomato, onion on brioche with steakhouse chips
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Southern Chicken Burger 20</p>
-              <p>
-                Lettuce, cucumber pickles, red pickles, cheese, chipotle mayo on
-                brioche with steakhouse chips
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Fish Burger 20</p>
-              <p>
-                Crispy Hoki, lettuce, cucumber pickles, tomato, siracha mayo on
-                brioche with steakhouse chips
-              </p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Schitzel sandwich 16.5</p>
-              <p>Lettuce, tomato, aioli on Turkish bread</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Pancakes berries 18</p>
-              <p>Yoghurt berry, berries and raspberry coulis</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Bowl of chips 8</p>
-              <p>with aioli & tomato sauce</p>
-              <br />
-              <p style={{ fontWeight: "bold" }}>Sweet potato fries 10</p>
-              <p>with sour cream & sweet chilli</p>
-              <br />
             </div>
           </div>
-        )}
-        {/**Caterring Menu */}
-        {!isChecked && (
-          <div className="foodMenu">
-            <div className="basis-1/4 secTitile">Catering Menu</div>
-            <div className="basis-3/4 foodSection">{caterringMenu()}</div>{" "}
+          <div className="about-us-section">
+            <div className="basis-3/5 flex justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                Quality coffee & food can brighten your day, and a quality
+                espresso bar can brighten the world. Born from a mission to
+                pioneer what is possible in an industry moving at incredible
+                speed, Lumos is positioned to light the way.
+              </p>
+            </div>
+            <div className="basis-2/5 flex justify-center content-center">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-2.jpg"
+                alt="image description"
+              />
+            </div>
           </div>
-        )}
+          <div className="about-us-section">
+            <div className="basis-2/5 flex justify-center content-center">
+              <img
+                class="h-auto max-w-lg rounded-3xl	"
+                src="/LUMOS-3.jpg"
+                alt="image description"
+              />
+            </div>
+            <div className="basis-3/5 flex justify-center content-center">
+              <p className="w-2/3 text-lg font-semibold">
+                Leaving no part of our process in the shadows, our sustainable
+                approach shines a light on every person, product, and piece of
+                packaging involved from crop to cup. We embrace a responsibility
+                to deliver a transcendent sensory experience, whilst ensuring
+                our state-of-the-art setup and approach creates a better world
+                and lightens your day.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="relative inline-block w-10 align-middle select-none transition duration-200 ease-in">
-        <input
-          type="checkbox"
-          name="toggle"
-          id="toggle"
-          class="toggle-checkbox hidden"
-          checked={isChecked} // Use the state value
-          onChange={handleToggleChange} // Update state on change
-        />
-        <label
-          for="toggle"
-          class="toggle-label block overflow-hidden h-6 rounded-full cursor-pointer"
-        ></label>
-      </div>
-
-      {isChecked ? (
-        <p style={{ fontWeight: "bolder", margin: "10px" }}>
-          Toggle to Catering Menu
-        </p>
-      ) : (
-        <p style={{ fontWeight: "bolder", margin: "10px" }}>
-          Toggle to Normal Menu
-        </p>
-      )}
     </main>
   );
 }
